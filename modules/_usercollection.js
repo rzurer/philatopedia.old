f2f6ba0cfcc433e5ca33e21b7403650a7bf3b1c2
Robert Zurer <robert@zurer.com>
2012-08-05 06:43:52 -0400

    put controls back

--- modules/_usercollection.js
+++ modules/_usercollection.js
@@ -9,63 +9,62 @@ exports.privateMembers = {
 		this.common = common;
 		this.router = router;
 	},
-	initializeControls : function (Controls) {
-		this.controls = Controls;
-		// this.getSubmitToSandboxActions = controls.getSubmitToSandboxActions;
-		// this.getSubmitToSandboxAction = controls.getSubmitToSandboxAction;
-		// this.getDeleteStampActions = controls.getDeleteStampActions;
-		// this.getDeleteStampAction = controls.getDeleteStampAction;
-		// this.getStampContainer = controls.getStampContainer;
-		// this.getStampImages = controls.getStampImages;
-		// this.getStampLabels = controls.getStampLabels;
-		// this.getStampListings = controls.getStampListings;
-		// this.listings = controls.listings;
-		// this.collectionsource = controls.collectionsource;
-		// this.clearSearchControl = controls.clearSearchControl;
-		// this.doSearchControl = controls.doSearchControl;
-		// this.toaster = controls.toaster;
-		// this.window = controls.window;
-		// this.getStampId = controls.getStampId;
-		// this.getStampIdForAction = controls.getStampIdForAction;
-		// this.getStampImage = controls.getStampImage;
-		// this.nostampImage = controls.nostampImage;
-		// this.tagsource = controls.tagControls.tagsource;
-		// this.addLocalTagControl = controls.tagControls.addLocalTagControl;
-		// tags.initializeControls(controls.tagControls);
-		// arr = controls.array;
-		// this.search.initializeControls(controls.searchControls);
+	initializeControls : function (controls) {
+		this.getSubmitToSandboxActions = controls.getSubmitToSandboxActions;
+		this.getSubmitToSandboxAction = controls.getSubmitToSandboxAction;
+		this.getDeleteStampActions = controls.getDeleteStampActions;
+		this.getDeleteStampAction = controls.getDeleteStampAction;
+		this.getStampContainer = controls.getStampContainer;
+		this.getStampImages = controls.getStampImages;
+		this.getStampLabels = controls.getStampLabels;
+		this.getStampListings = controls.getStampListings;
+		this.listings = controls.listings;
+		this.collectionsource = controls.collectionsource;
+		this.clearSearchControl = controls.clearSearchControl;
+		this.doSearchControl = controls.doSearchControl;
+		this.toaster = controls.toaster;
+		this.window = controls.window;
+		this.getStampId = controls.getStampId;
+		this.getStampIdForAction = controls.getStampIdForAction;
+		this.getStampImage = controls.getStampImage;
+		this.nostampImage = controls.nostampImage;
+		this.tagsource = controls.tagControls.tagsource;
+		this.addLocalTagControl = controls.tagControls.addLocalTagControl;
+		tags.initializeControls(controls.tagControls);
+		arr = controls.array;
+		this.search.initializeControls(controls.searchControls);
 	},
 	setAutoCompletes : function () {
-		this.picklists.setTagsAutocomplete(this.controls.tagsource, false);
-		this.picklists.setCollectionsAutocomplete(this.controls.collectionsource, false);
+		this.picklists.setTagsAutocomplete(this.tagsource, false);
+		this.picklists.setCollectionsAutocomplete(this.collectionsource, false);
 		//this.picklists.setCountriesAutocomplete(false);
 	},
 	clearSearchEntries : function () {
-		this.controls.collectionsource.val(null);
-		this.controls.tagsource.val(null);
+		this.collectionsource.val(null);
+		this.tagsource.val(null);
 	},
 	setImages : function () {
 		var i, k, stampId, defaultImageSrc, img, image, stampImages;
 		for (i = 0; i < arr.length; i += 1) {
 			stampId = arr[i].stampId;
 			defaultImageSrc = arr[i].defaultImageSrc;
-			img = this.controls.getStampImage(stampId);
+			img = this.getStampImage(stampId);
 			img.attr('src', defaultImageSrc);
 			if (img.attr('src') && img.attr('src').length === 0) {
-				img.attr('src', that.controls.nostampImage);
+				img.attr('src', that.nostampImage);
 			}
 		}
 		stampImages = this.getStampImages();
 		for (k = 0; k < stampImages.length; k += 1) {
 			image = stampImages[k];
 			if (!image.getAttribute('src') || image.getAttribute('src').length === 0) {
-				image.setAttribute('src', that.controls.nostampImage);
+				image.setAttribute('src', that.nostampImage);
 			}
 		}
 	},
 	truncate : function (callback) {
 		var value, truncated, labels;
-		labels = that.controls.getStampLabels();
+		labels = that.getStampLabels();
 		labels.each(function () {
 			if (this.innerText.length > 30) {
 				value = this.innerText;
@@ -79,7 +78,7 @@ exports.privateMembers = {
 		}
 	},
 	cleanup : function (html, callback) {
-		that.controls.listings.html(html);
+		that.listings.html(html);
 		that.setImages();
 		tags.deleteLocalTags();
 		that.clearSearchEntries();
@@ -94,24 +93,24 @@ exports.privateMembers = {
 	},
 	submitToSandbox : function () {
 		var stampId, callback, target;
-		stampId = that.controls.getStampIdForAction(this);
-		target = that.controls.getSubmitToSandboxAction(this);
+		stampId = that.getStampIdForAction(this);
+		target = that.getSubmitToSandboxAction(this);
 		callback = function () {
-			that.common.showToaster(target, that.controls.toaster, "copied");
+			that.common.showToaster(target, that.toaster, "copied");
 		};
 		that.router.submitToSandbox(stampId, that.isValid, callback);
 	},
 	goToStamp : function () {
-		that.router.goToStamp(that.controls.getStampId(this));
+		that.router.goToStamp(that.getStampId(this));
 	},
 	deleteStamp : function () {
 		var stampId, target, deleteStampAction, deletedStampContainer, removeContainers, delayedRemoveContainers;
 		target = this;
-		stampId = that.controls.getStampIdForAction(target);
+		stampId = that.getStampIdForAction(target);
 		that.router.deleteStamp(stampId, function (data) {
 			if (data.success) {
-				deletedStampContainer = that.controls.getStampContainer(target);
-				deleteStampAction = that.controls.getDeleteStampAction(target);
+				deletedStampContainer = that.getStampContainer(target);
+				deleteStampAction = that.getDeleteStampAction(target);
 				removeContainers =  function () {
 					deletedStampContainer.remove();
 					deleteStampAction.remove();
@@ -119,29 +118,29 @@ exports.privateMembers = {
 				delayedRemoveContainers = function () {
 					setTimeout(removeContainers, 1200);
 				};
-				that.common.showToaster(deleteStampAction, that.controls.toaster, "deleting ...", delayedRemoveContainers);
+				that.common.showToaster(deleteStampAction, that.toaster, "deleting ...", delayedRemoveContainers);
 			}
 		});
 	},
 	assignEventHandlers : function (argument) {
-		this.controls.getSubmitToSandboxActions().click(this.submitToSandbox);
-		this.controls.getDeleteStampActions().click(this.deleteStamp);
-		this.controls.getStampListings().click(this.goToStamp);
-		this.controls.clearSearchControl.click(this.clearSearch);
-		this.controls.tagsource.blur(this.leaveTag);
-		this.controls.addLocalTagControl.click(this.addLocalTag);
-		this.controls.doSearchControl.click(this.createQueryAndSearch);
+		this.getSubmitToSandboxActions().click(this.submitToSandbox);
+		this.getDeleteStampActions().click(this.deleteStamp);
+		this.getStampListings().click(this.goToStamp);
+		this.clearSearchControl.click(this.clearSearch);
+		this.tagsource.blur(this.leaveTag);
+		this.addLocalTagControl.click(this.addLocalTag);
+		this.doSearchControl.click(this.createQueryAndSearch);
 	},
 	setSearchControls : function () {
 		var that = this,
 			collectionsCallback = function (collections) {
 				if (collections.length === 0) {
-					that.controls.collectionsource.attr("disabled", "disabled");
+					that.collectionsource.attr("disabled", "disabled");
 				}
 			},
 			tagsCallback = function (tags) {
 				if (tags.length === 0) {
-					that.controls.tagsource.attr("disabled", "disabled");
+					that.tagsource.attr("disabled", "disabled");
 				}
 			};
 		this.picklists.getCollections(collectionsCallback);
@@ -168,13 +167,13 @@ exports.privateMembers = {
 		});
 	},
 	ready : function () {
-		this.controls.listings.hide();
+		this.listings.hide();
 		this.setSearchControls();
 		this.setAutoCompletes();
 		this.assignEventHandlers();
 		this.search.filterStampListings(function () {
 			that.cleanup(function () {
-				that.controls.listings.show();
+				that.listings.show();
 			});
 		});
 	}
