cfb033e6d4e7fb15e244b1ca27c3261aeb0a7554
Robert Zurer <robert@zurer.com>
2012-09-02 06:45:31 -0400

    added and renamed tests

---
+++ test/constants.js
@@ -0,0 +1,10 @@
+/*global  describe, it*/
+"use strict";
+var sut = require('../modules/constants').constants,
+	assert = require('assert');
+describe('Urls', function () {
+    it("should contain expected constants", function () {
+        assert.strictEqual('Click to satisfy your stamp lust', sut.loggedInSplashTitle);
+        assert.strictEqual('Sign in to satisfy your stamp lust', sut.loggedOutSplashTitle);
+    });
+});
